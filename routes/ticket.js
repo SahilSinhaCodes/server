5963c43317820fa6aa376be8ab5341bb66885684
sahilsinhacodes <sinhasahil423@gmail.com>
2025-06-16 14:36:48 +0530

    Update ticket.js

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,26 +1,3 @@
-//import express from "express";
-//import authMiddleware from "../middleware/authMiddleware.js";
-//import {
-//  createTicket,
-//  getTicketsByProject,
-//  updateTicket,
-//  deleteTicket,
-//  assignTicket,
-//} from "../controllers/ticketController.js";
-//
-//const router = express.Router();
-//
-//router.use(authMiddleware);
-//
-//// CRUD routes
-//router.post("/", createTicket);                    // Create ticket
-//router.get("/:projectId", getTicketsByProject);    // List tickets by project
-//router.put("/:id", updateTicket);                  // Update ticket
-//router.delete("/:id", deleteTicket);               // Delete ticket
-//router.put("/:id/assign", assignTicket);           // Assign ticket
-//
-//export default router;
-
 import express from 'express';
 import {
   createTicket,
